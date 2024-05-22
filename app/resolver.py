00a44e8cc54309ada4450f1ccd2d2a3b7e733745
Moon Hyunbin <125638803+cleanbin98@users.noreply.github.com>
2024-05-22 16:25:59 +0900

    Update resolver.py

--- app/resolver.py
+++ app/resolver.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 item_fname = 'data/movies_final.csv'
 
-
+#RANDOM 영화 반환
 def random_items():
     movies_df = pd.read_csv(item_fname)
     movies_df = movies_df.fillna('')
     result_items = movies_df.sample(n=10).to_dict("records")
     return result_items
 
+#RANDOM 장르의 영화 반환
 def random_genres_items(genre):
     movies_df = pd.read_csv(item_fname)
     genre_df = movies_df[movies_df['genres'].apply(lambda x: genre in x.lower())]
